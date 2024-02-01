cf3a50b3872f1484f193d90f1406862c6b611cbd
ivanlim.choonkit <ivanlim608@gmail.com>
2024-02-01 23:07:26 +0800

    Remove boiler plate

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
+﻿
 
-namespace Dotnet{
-    class Program{
-        static void Main(string[] args){
-            Console.WriteLine("Hello World");
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-        }
-    }
-}
+Console.WriteLine("Hello World");
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
