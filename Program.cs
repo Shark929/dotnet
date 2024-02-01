22d1987730767925a0ccf108fc7f8fefd1b0c50e
ivanlim.choonkit <ivanlim608@gmail.com>
2024-02-01 23:07:10 +0800

    - Get user input using readline - remove boiler plate

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ namespace Dotnet{
     class Program{
         static void Main(string[] args){
             Console.WriteLine("Hello World");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
