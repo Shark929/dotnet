fbaf68ae4daf945ffcec847e77b070230aa3eac1
ivanlim.choonkit <ivanlim608@gmail.com>
2024-02-02 10:22:16 +0800

    This is a tutorial for variables and data type

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
-﻿
+﻿int i = 10;
+float f = 2.0f;
+decimal d = 10.0m;
+bool b = true;
+char c = 'C';
 
-Console.WriteLine("Hello World");
-Console.WriteLine("What is your name?");
-string name = Console.ReadLine();
-Console.WriteLine("Hello " + name);
+string str = "a collection of characters";
+
+var x = 10;
+var z = "Hello!";
+
+int[] vals = new int[5];
+string[] strs = ["one", "two", "three"];
+
+// Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}", i, f, d, b, c, str, x, z, vals, strs);
+object obj = null;
+Console.WriteLine(obj);
